4887f04f0b7fabf166d8d4b926800f17d388b5ae
DP <davefreew.9@gmail.com>
2025-09-19 22:11:47 -0400

    Added reset button

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -10,11 +10,19 @@ function increaseCount() {
     checkCountValue();
 }
 
+function resetCount() {
+    count=0;
+    displayCount();
+    checkCountValue();
+}
+
 function checkCountValue() {
     if (count === 10) {
         alert("Your Instagram post gained 10 followers! Congratulations!");
     } else if (count === 20) {
         alert("Your Instagram post gained 20 followers! Keep it up!");
+    } else if (count === 0) {
+        alert("Your Instagram followers have been reset");
     }
 }
 
